dd868dace90f515131aff6bf6bb86f694796c67f
Marck Christian Agulto <150341453+m1sskeiso@users.noreply.github.com>
2024-01-20 18:00:53 +0700

    def function

--- multiplicating_adding_numbers.py
+++ multiplicating_adding_numbers.py
@@ -1,6 +1,12 @@
+
+
 # The given
 number1 = 40
 number2 = 30
+
 # Define a function to add two numbers
+def add(number1, number2):
+    return (number1 + number2)
+
 # Call the add function and store the result
 # Print the result
