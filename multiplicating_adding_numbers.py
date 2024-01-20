a23cb8f3b1db73a0d26f0dc788d4d5e804e2a177
Marck Christian Agulto <150341453+m1sskeiso@users.noreply.github.com>
2024-01-20 18:11:26 +0700

    fix error

--- multiplicating_adding_numbers.py
+++ multiplicating_adding_numbers.py
@@ -1,14 +1,16 @@
 
-# The given
+# Given numbers
 number1 = 40
 number2 = 30
 
 # Define a function to add two numbers
 def add(number1, number2):
-    return (number1 + number2)
+    return number1 + number2  
 
 # Call the add function and store the result
-result = (number1 +number2)
+result = add(number1, number2)  
 
 # Print the result
-print("the result is", result)
+print("The result is", result)
+
+
