630fec328b1343611e430714cf3738181064df9d
Marck Christian Agulto <150341453+m1sskeiso@users.noreply.github.com>
2024-01-20 18:19:29 +0700

    the whole code its easy to put so i put it straight forward

--- multiplicating_numbers.py
+++ multiplicating_numbers.py
@@ -1,4 +1,13 @@
 # Given numbers
+number1 = 20
+number2 = 30
+
 # Define a function to multiply two numbers
+def multiply(number1, number2):
+    return number1 * number2
+
 # Call the multiply function and store the result
+result = number1 * number2
+
 # Print the result
+print("the result is", result)
